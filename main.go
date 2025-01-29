5e05db7a39a82c2cd49e25f526fbabacaab92171
rapoluvinay-hub <rapolu.vinay0703@gmail.com>
2025-01-28 20:15:20 -0500

    Update main.go

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"receipt-processor/handlers"
+)
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/receipts/process", handlers.ProcessReceipts).Methods("POST")
+	r.HandleFunc("/receipts/{id}/points", handlers.GetPoints).Methods("GET")
+
+	log.Println("Server is running on port 8080...")
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
